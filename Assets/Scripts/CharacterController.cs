39e1cba8494dcf156a7d32986e6d5d925ac2e297
Roger Bucio <Roger.Bucio@iam.local>
2018-11-14 15:16:52 -0600

    Idle and Skip animation & Player Movement Part 1

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+    [SerializeField]
+    private float maxSpeed = 10f;
+    [SerializeField]
+    private Rigidbody2D rigidbody2DInstance;
+    [SerializeField]
+    private float accelerationForce = 5;
+
+    private float horizontalInput;
+    private bool isFacingRight;
+
+    Animator anim;
+	// Use this for initialization
+	void Start () {
+
+        anim = GetComponent<Animator>();
+
+
+	}
+    void Update()
+    {
+        UpdateHorizontalInput();
+
+
+    }
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        Move();
+
+	}
+    private void UpdateHorizontalInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+    }
+    private void Move()
+    {
+        rigidbody2DInstance.AddForce(Vector2.right * horizontalInput * accelerationForce);
+        Vector2 clampedVelocity = rigidbody2DInstance.velocity;
+        clampedVelocity.x = Mathf.Clamp(rigidbody2DInstance.velocity.x, -maxSpeed, maxSpeed);
+        rigidbody2DInstance.velocity = clampedVelocity;
+        if (horizontalInput < 0 && !isFacingRight)
+            Flip();
+        else if (horizontalInput > 0 && isFacingRight)
+            Flip();
+    }
+    void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
